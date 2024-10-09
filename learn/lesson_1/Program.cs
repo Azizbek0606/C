0f7e49bd1831d38dcce3ca94c8fbd881d8fa59b6
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-10-09 22:15:11 +0500

    update

---
+++ learn/lesson_1/Program.cs
@@ -0,0 +1,27 @@
+﻿class Project{
+    static void Main(string[] args){
+        // if (shart){
+        //     amal;
+        // }else if(shart){
+        //     amal;
+        // }else{
+        //     amal;
+        // }
+
+        // switch (shart){
+        //     case 1:
+        //         amal;
+        //         break;
+        //     case 2:
+        //         amal;
+        //         break;
+        //     default:
+        //         amal;
+        //         break;
+        // }
+
+        // for(int i = 0; i < 5; i++){
+        //     amal;
+        // }
+    }
+}
